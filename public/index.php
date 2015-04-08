e1f46a2b5afa99d6eeffe8cdf0f36e0abc298b9e
ImpulseChimp <cmw2379@vt.edu>
2015-04-08 15:18:52 -0400

    test push

--- public/index.php
+++ public/index.php
@@ -1,34 +1,21 @@
 <?php
 
-//Include all routes on site
-foreach (glob("../app/routes/*.php") as $filename)
-{
-    include $filename;
-}
-
-foreach (glob("../app/controllers/*.php") as $filename)
-{
-    include $filename;
-}
-
-//Create routes and initialize routes
-$router = new \Phalcon\Mvc\Router();
-
-$router->mount(new PublicRoutes());
-$router->mount(new ApiRoutes());
-$router->mount(new AdminRoutes());
-
-$router->handle();
-
-$controller = $router->getControllerName();
-$action = $router->getActionName();
-$params = $router->getParams();
+// Use Loader() to autoload our model
+$loader = new \Phalcon\Loader();
 
 $di = new \Phalcon\DI\FactoryDefault();
 
-$d = new Phalcon\Mvc\Dispatcher();
-$d->setDI($di);
-$d->setControllerName($router->getControllerName());
-$d->setActionName($router->getActionName());
-$d->setParams($router->getParams());
+$router = new \Phalcon\Mvc\Micro($di);
+
+
+$router->get('/api/handler/{model}/{params}', function($model, $params){
+
+	echo 'TEST RUN';
+	echo $model;
+	echo $params;
+});
+
+
+$router->handle();
+
 
