83944202c5f4d0a1573719c079cbc0de0174e6b4
ImpulseChimp <cmw2379@vt.edu>
2015-04-11 13:42:21 -0400

    test push

--- public/index.php
+++ public/index.php
@@ -1,86 +1,77 @@
 <?php
 
-use \Phalcon\Mvc\Dispatcher;
+echo "TEST TEST DEST";
 
-try {
-
-    //Register an autoloader
-    $loader = new \Phalcon\Loader();
-    $loader->registerDirs(array(
-        '../app/controllers/',
-        '../app/models/'
-    ))->register();
-
-    //Create a DI
-    $di = new Phalcon\DI\FactoryDefault();
-
-    //Setup the view component
-    $di->set('view', function(){
-        $view = new \Phalcon\Mvc\View();
-        $view->setViewsDir('../app/views/');
-        return $view;
-    });
-
-    // $host = gethostname();
-    //Setup the database service
-    // $di->set('db', function(){
-    //     return new \Phalcon\Db\Adapter\Pdo\Mysql(array(
-    //         "host" => $host,
-    //         "username" => "addhawk",
-    //         "password" => "addhawk4784",
-    //         "dbname" => "addhawk"
-    //     ));
-    // });
-
-    $di->set(
-    'dispatcher',
-    function() use ($di) {
-        $eventsManager = $di->getShared('eventsManager');
-        $eventsManager->attach(
-            'dispatch:beforeException',
-            function($event, $dispatcher, $exception) {
-                switch ($exception->getCode()) {
-                    case Dispatcher::EXCEPTION_HANDLER_NOT_FOUND:
-                    case Dispatcher::EXCEPTION_ACTION_NOT_FOUND:
-                        $dispatcher->forward(
-                            array(
-                                'controller' => 'error',
-                                'action' => 'error404',
-                            )
-                        );
-                        return false;
-                        break; // for checkstyle
-                    default:
-                        $dispatcher->forward(
-                            array(
-                                'controller' => 'error',
-                                'action' => 'error404',
-                            )
-                        );
-                        return false;
-                        break; // for checkstyle
-                }
-            }
-        );
-        $dispatcher = new Dispatcher();
-        $dispatcher->setEventsManager($eventsManager);
-        return $dispatcher;
-    },
-    true
-);
-
-    //Setup a base URI so that all generated URIs include the "tutorial" folder
-    $di->set('url', function(){
-        $url = new \Phalcon\Mvc\Url();
-        $url->setBaseUri('/');
-        return $url;
-    });
-
-    //Handle the request
-    $application = new \Phalcon\Mvc\Application($di);
-
-    echo $application->handle()->getContent();
-
-} catch(\Phalcon\Exception $e) {
-     echo "PhalconException: ", $e->getMessage();
-}
+// use \Phalcon\Mvc\Dispatcher;
+//
+// try {
+//
+//     //Register an autoloader
+//     $loader = new \Phalcon\Loader();
+//     $loader->registerDirs(array(
+//         '../app/controllers/',
+//         '../app/models/'
+//     ))->register();
+//
+//     //Create a DI
+//     $di = new Phalcon\DI\FactoryDefault();
+//
+//     //Setup the view component
+//     $di->set('view', function(){
+//         $view = new \Phalcon\Mvc\View();
+//         $view->setViewsDir('../app/views/');
+//         return $view;
+//     });
+//
+//     $di->set(
+//     'dispatcher',
+//     function() use ($di) {
+//         $eventsManager = $di->getShared('eventsManager');
+//         $eventsManager->attach(
+//             'dispatch:beforeException',
+//             function($event, $dispatcher, $exception) {
+//                 switch ($exception->getCode()) {
+//                     case Dispatcher::EXCEPTION_HANDLER_NOT_FOUND:
+//                     case Dispatcher::EXCEPTION_ACTION_NOT_FOUND:
+//                         $dispatcher->forward(
+//                             array(
+//                                 'controller' => 'error',
+//                                 'action' => 'error404',
+//                             )
+//                         );
+//                         return false;
+//                         break; // for checkstyle
+//                     default:
+//                         $dispatcher->forward(
+//                             array(
+//                                 'controller' => 'error',
+//                                 'action' => 'error404',
+//                             )
+//                         );
+//                         return false;
+//                         break; // for checkstyle
+//                 }
+//             }
+//         );
+//         $dispatcher = new Dispatcher();
+//         $dispatcher->setEventsManager($eventsManager);
+//         return $dispatcher;
+//     },
+//     true
+// );
+//
+//     //Setup a base URI so that all generated URIs include the "tutorial" folder
+//     $di->set('url', function(){
+//         $url = new \Phalcon\Mvc\Url();
+//         $url->setBaseUri('/');
+//         return $url;
+//     });
+//
+//     //Handle the request
+//     $application = new \Phalcon\Mvc\Application($di);
+//
+//     echo $application->handle()->getContent();
+//
+// } catch(\Phalcon\Exception $e) {
+//      echo "PhalconException: ", $e->getMessage();
+// }
