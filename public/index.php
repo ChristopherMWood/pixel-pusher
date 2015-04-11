3a70026c73752210554ef8548b54a26a5c7f41e5
ImpulseChimp <cmw2379@vt.edu>
2015-04-11 13:47:24 -0400

    test push again

--- public/index.php
+++ public/index.php
@@ -22,44 +22,44 @@ try {
         $view->setViewsDir('../app/views/');
         return $view;
     });
-//
-//     $di->set(
-//     'dispatcher',
-//     function() use ($di) {
-//         $eventsManager = $di->getShared('eventsManager');
-//         $eventsManager->attach(
-//             'dispatch:beforeException',
-//             function($event, $dispatcher, $exception) {
-//                 switch ($exception->getCode()) {
-//                     case Dispatcher::EXCEPTION_HANDLER_NOT_FOUND:
-//                     case Dispatcher::EXCEPTION_ACTION_NOT_FOUND:
-//                         $dispatcher->forward(
-//                             array(
-//                                 'controller' => 'error',
-//                                 'action' => 'error404',
-//                             )
-//                         );
-//                         return false;
-//                         break; // for checkstyle
-//                     default:
-//                         $dispatcher->forward(
-//                             array(
-//                                 'controller' => 'error',
-//                                 'action' => 'error404',
-//                             )
-//                         );
-//                         return false;
-//                         break; // for checkstyle
-//                 }
-//             }
-//         );
-//         $dispatcher = new Dispatcher();
-//         $dispatcher->setEventsManager($eventsManager);
-//         return $dispatcher;
-//     },
-//     true
-// );
-//
+
+    $di->set(
+    'dispatcher',
+    function() use ($di) {
+        $eventsManager = $di->getShared('eventsManager');
+        $eventsManager->attach(
+            'dispatch:beforeException',
+            function($event, $dispatcher, $exception) {
+                switch ($exception->getCode()) {
+                    case Dispatcher::EXCEPTION_HANDLER_NOT_FOUND:
+                    case Dispatcher::EXCEPTION_ACTION_NOT_FOUND:
+                        $dispatcher->forward(
+                            array(
+                                'controller' => 'error',
+                                'action' => 'error404',
+                            )
+                        );
+                        return false;
+                        break; // for checkstyle
+                    default:
+                        $dispatcher->forward(
+                            array(
+                                'controller' => 'error',
+                                'action' => 'error404',
+                            )
+                        );
+                        return false;
+                        break; // for checkstyle
+                }
+            }
+        );
+        $dispatcher = new Dispatcher();
+        $dispatcher->setEventsManager($eventsManager);
+        return $dispatcher;
+    },
+    true
+);
+
 //     //Setup a base URI so that all generated URIs include the "tutorial" folder
 //     $di->set('url', function(){
 //         $url = new \Phalcon\Mvc\Url();
