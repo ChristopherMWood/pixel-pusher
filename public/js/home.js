4b58252b05bdc70715b5611e05fc0046a879c6ee
Dillon Gresham <dillon7@vt.edu>
2015-04-13 11:24:24 -0400

    i did it again

--- public/js/home.js
+++ public/js/home.js
@@ -35,7 +35,6 @@ function displayPPInfo() {
 }
 
 function infoClicked() {
-	document.getElementById("settingsDropDownDiv").style.display = "block";
 	document.getElementById("bg").className = "infoBody";
 	document.getElementById("ppDiv").style.display = "none";
 	document.getElementById("infoDiv").style.display = "none";
@@ -46,14 +45,14 @@ function infoClicked() {
 	var rowDD = document.getElementById("rowDD");
 	var seatDD = document.getElementById("seatDD");
 
-	sectionDD.change(function () {
+	$("#sectionDD").change(function () {
 		// when a choice is selected, display the drop down box for rows if needed
 		if (rowDD.style.display == "none") {
 			rowDD.style.display = "block";
 		}
 	});
-	
-	rowDD.change(function () {
+
+	$("#rowDD").change(function () {
 		// when a choice is selected, display the drop down box for rows if needed
 		if (seatDD.style.display == "none") {
 			seatDD.style.display = "block";
