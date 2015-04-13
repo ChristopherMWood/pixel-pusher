fc85ad3dab2f3720fa80b75b3c6193063e011e8a
Dillon Gresham <dillon7@vt.edu>
2015-04-13 19:42:43 -0400

    dillon's changes

--- public/js/home.js
+++ public/js/home.js
@@ -39,22 +39,20 @@ function displayPPInfo() {
 	}
 }
 
-function infoClicked() {
-	//Doesn't work
-	//$(window).unbind('load', transitionBg);
-	//transitionBg(false);
-	
-
-	document.getElementById("bg").className = "infoBody";
+function settingsClicked() {
+	// change the screen to display the correct elements
+	document.getElementById("bg").className = "settingsBody";
 	document.getElementById("ppDiv").style.display = "none";
 	document.getElementById("infoDiv").style.display = "none";
 	document.getElementById("settingsDiv").style.display = "none";
+	document.getElementById("settingsTitleDiv").style.display = "block";
+	document.getElementById("innerSectionDiv").style.display = "inline-block";
+	document.getElementById("tableGridDiv").style.display = "inline-block";
 
-	
-	document.getElementById("innerSectionDiv").style.display = "block";
 	var sectionDiv = document.getElementById("sectionDiv");
 	var rowDiv = document.getElementById("rowDiv");
 	var seatDiv = document.getElementById("seatDiv");
+	var rowVal;
 	sectionDiv.style.display = "block";
 
 	$("#sectionDD").change(function () {
@@ -69,18 +67,24 @@ function infoClicked() {
 		// when a choice is selected, display the drop down box for seats if needed
 		if (seatDiv.style.display == "none") {
 			seatDiv.style.display = "block";
-			setDDText($("#rowDD").val(), 1);
+			rowVal = $("#rowDD").val();
+			highlightRow(rowVal);
+			setDDText(rowVal, 1);
 		}
 	});
 
 	$("#seatDD").change(function () {
 		// when a choice is selected, change to a text field
+		highlightSeat(rowVal, $("#seatDD").val());
 		setDDText($("#seatDD").val(), 2);
 	});
+
+	// fill the table div with the correct HTML based on table size
+	document.getElementById("tableGridDiv").innerHTML = createSettingsTable(10, 10);
 }
 
-function settingsClicked() {
-	document.getElementById("bg").className = "settingsBody";
+function infoClicked() {
+	document.getElementById("bg").className = "infoBody";
 	document.getElementById("ppDiv").style.display = "none";
 	document.getElementById("infoDiv").style.display = "none";
 	document.getElementById("settingsDiv").style.display = "none";
@@ -110,4 +114,38 @@ function setDDText(ddString, dropDownNum) {
 	// clear the dropdown's Div and then create a text field
 	ddDiv.innerHTML = "";
 	ddDiv.innerHTML = "<h name='" + ddName + "Text' id='" + ddName + "Text'>" + resultString + "</h>";
+}
+
+function createSettingsTable(width, height) {
+	var tableString = "<table name='settingsTable' id='settingsTable'>";
+
+	for (var i = 0; i < width; i++) {
+
+		// id is row_ and then the row number
+		tableString += "<tr id='row_" + i + "' name='row_" + i + "'>";
+		for (var j = 0; j < height; j++) {
+
+			// id is seat_ and then the row number, another _, and then the seat number
+			tableString += "<td id='seat_" + i + "_" + j + "' name='seat_" + i + "_" + j + "'></td>";
+		}
+		tableString += "</tr>";
+	}
+	tableString += "</table>";
+
+	return tableString;
+}
+
+function highlightRow(rowNum) {
+	var rowName = "row_" + rowNum;
+	document.getElementById(rowName).style.backgroundColor = "red";
+}
+
+function highlightSeat(rowNum, seatNum) {
+	var seatName = "seat_" + rowNum + "_" + seatNum;
+	console.log(seatName);
+	// get the row name to unhighlight it
+	var rowName = "row_" + rowNum;
+	document.getElementById(rowName).style.backgroundColor = "white";
+
+	document.getElementById(seatName).style.backgroundColor = "red";
 }
