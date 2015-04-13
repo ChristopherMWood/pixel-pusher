91617a98f4114571cdb543cd00ae25c80a96c829
ImpulseChimp <cmw2379@vt.edu>
2015-04-13 15:25:47 -0400

    asdf

--- public/index.php
+++ public/index.php
@@ -1,34 +1,71 @@
 <?php
-
-use Phalcon\Loader,
-    Phalcon\DI\FactoryDefault,
-    Phalcon\Mvc\Application,
-    Phalcon\Mvc\View;
-
-$loader = new Loader();
-
-$loader->registerDirs(
-    array(
-        '../apps/controllers/',
-        '../apps/models/'
-    )
-)->register();
-
-$di = new FactoryDefault();
-
-// Registering the view component
-$di->set('view', function() {
-    $view = new View();
-    $view->setViewsDir('../apps/views/');
-    return $view;
-});
-
+use \Phalcon\Mvc\Dispatcher;
 try {
+    //Register an autoloader
+    $loader = new \Phalcon\Loader();
+    $loader->registerDirs(array(
+                '../app/controllers/',
+                '../app/models/'
+    ))->register();
 
-    $application = new Application($di);
-
-    echo $application->handle()->getContent();
-
-} catch (\Exception $e) {
-    echo $e->getMessage();
+    //Create a DI
+    $di = new Phalcon\DI\FactoryDefault();
+    //Setup the view component
+    $di->set('view', function(){
+        $view = new \Phalcon\Mvc\View();
+        $view->setViewsDir('../app/views/');
+        return $view;
+    });
+    //Setup the database service
+    $di->set('db', function(){
+        return new \Phalcon\Db\Adapter\Pdo\Mysql(array(
+            "host" => "localhost",
+            "username" => "addhawk",
+            "password" => "addhawk4784",
+            "dbname" => "addhawk"
+        ));
+    });
+    $di->set(
+    'dispatcher',
+    function() use ($di) {
+        $eventsManager = $di->getShared('eventsManager');
+        $eventsManager->attach(
+            'dispatch:beforeException',
+            function($event, $dispatcher, $exception) {
+                switch ($exception->getCode()) {
+                    case Dispatcher::EXCEPTION_HANDLER_NOT_FOUND:
+                    case Dispatcher::EXCEPTION_ACTION_NOT_FOUND:
+                        $dispatcher->forward(
+                            array(
+                                'controller' => 'error',
+                                'action' => 'error404',
+                            )
+                        );
+                        return false;
+                        break; // for checkstyle
+                    default:
+                        $dispatcher->forward(
+                            array(
+                                'controller' => 'error',
+                                'action' => 'error404',
+                            )
+                        );
+                        return false;
+                        break; // for checkstyle
+                }
+            }
+        );
+        $dispatcher = new Dispatcher();
+        $dispatcher->setEventsManager($eventsManager);
+        return $dispatcher;
+    },
+    true
+);
+    //Handle the request
+    $app = new \Phalcon\Mvc\Application($di);
+    echo $app->handle()->getContent();
+} catch(\Phalcon\Exception $e) {
+     echo "PhalconException: ", $e->getMessage();
+     $logger = new \Phalcon\Logger\Adapter\File(__DIR__.'/../app/logs/runtime.log');
+     $logger->error($e->getMessage());
 }
