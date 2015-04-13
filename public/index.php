c49ec28db86ceb03945a3fe245b2b07af1d73e07
ImpulseChimp <cmw2379@vt.edu>
2015-04-13 15:11:39 -0400

    asdf

--- public/index.php
+++ public/index.php
@@ -1,29 +1,34 @@
 <?php
 
+use Phalcon\Loader,
+    Phalcon\DI\FactoryDefault,
+    Phalcon\Mvc\Application,
+    Phalcon\Mvc\View;
+
+$loader = new Loader();
+
+$loader->registerDirs(
+    array(
+        '../apps/controllers/',
+        '../apps/models/'
+    )
+)->register();
+
+$di = new FactoryDefault();
+
+// Registering the view component
+$di->set('view', function() {
+    $view = new View();
+    $view->setViewsDir('../apps/views/');
+    return $view;
+});
+
 try {
 
-    //Register an autoloader
-    $loader = new \Phalcon\Loader();
-    $loader->registerDirs(array(
-        '../app/controllers/',
-        '../app/models/'
-    ))->register();
-
-    //Create a DI
-    $di = new Phalcon\DI\FactoryDefault();
-
-    //Setup the view component
-    $di->set('view', function(){
-        $view = new \Phalcon\Mvc\View();
-        $view->setViewsDir('../app/views/');
-        return $view;
-    });
-
-    //Handle the request
-    $application = new \Phalcon\Mvc\Application($di);
+    $application = new Application($di);
 
     echo $application->handle()->getContent();
 
-} catch(\Phalcon\Exception $e) {
-     echo "PhalconException: ", $e->getMessage();
+} catch (\Exception $e) {
+    echo $e->getMessage();
 }
