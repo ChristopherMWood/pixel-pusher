c4da00140aa6c3de8eb8cdbf82137df00b5700a3
ImpulseChimp <cmw2379@vt.edu>
2015-04-13 20:30:04 -0400

    stuff

--- api/app/index.php
+++ api/app/index.php
@@ -1,14 +1,10 @@
 <?php
-
 // Use Loader() to autoload our model
 $loader = new \Phalcon\Loader();
-
 $loader->registerDirs(array(
-          '/models/'
+    __DIR__ . '/models/'
 ))->register();
-
 $di = new \Phalcon\DI\FactoryDefault();
-
 //Set up the database service
 $di->set('modelsManager', function(){
 	return new \Phalcon\Db\Adapter\Pdo\Mysql(array(
@@ -18,10 +14,8 @@ $di->set('modelsManager', function(){
 			"dbname" => "addhawk"
 	));
 });
-
 //Create new Micro application and router
 $router = new Phalcon\Mvc\Micro($di);
-
 /*
   This route MATCHES ALL VALID API REQUESTS to a model
   URL: :host/api/model/method/parameters
@@ -29,37 +23,30 @@ $router = new Phalcon\Mvc\Micro($di);
   PRE: $model and $method can only contain letter, numbers, '-', and '_' symbols
   POST: Valid JSON Response is returned
 */
-$router->map('/{model:[A-Za-z0-9_-]+}/{method:[A-Za-z0-9_-]+}/{parameters}', function($model, $method, $parameters) use ($router){
-
+$router->map('/{model:[A-Za-z0-9_-]+}/{method:[A-Za-z0-9_-]+}/*{parameters}', function($model, $method, $parameters) use ($router){
 	//Build request obj
 	include "lib/request.php";
 	$request = new Request($model, $method, $parameters);
-
 	//Prepare response obj
 	$response = new Phalcon\Http\Response();
-
 	$api_obj; //Pre define for if-else if block
-
 	//Call corresponding API below if possible
   if($model == "user") {
-		include "/lib/user_api.php";
+		include "lib/user_api.php";
 		$api_obj = new UserApi($request, $response, $router);
 		$response = $api_obj->executeRequest();
 	}
 	else if($model == "admin") {
-		include "/lib/admin_api.php";
+		include "lib/admin_api.php";
 		$api_obj = new AdminApi($request, $response, $router);
 		$response = $api_obj->executeRequest();
 	}
 	else {
 		$response->setStatusCode(405, "Model Not Found");
 	}
-
 	//Send response from api
 	$response->send();
 });
-
-
 /*
   The routes below catch all invalid api calls
   HTTP Methods: { GET, POST }
@@ -69,16 +56,9 @@ $router->map('/{model}/{method}', function($model, $method){
   echo "Model: ".$model."</br>";
   echo "Method: ".$method;
 });
-
 //Incomplete api call catch
 $router->map('/{model}', function($model){
   echo "PixelPusher API"."</br>";
   echo "Model: ".$model;
 });
-
-$router->notFound(function () use ($router) {
-    $router->response->setStatusCode(404, "Not Found")->sendHeaders();
-    echo 'This is crazy, but this page was not found! API 404';
-});
-
 $router->handle();
