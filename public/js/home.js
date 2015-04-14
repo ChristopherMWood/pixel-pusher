c9c2bfde18f135e7107d237a5ef5ad127c3fafd9
ImpulseChimp <cmw2379@vt.edu>
2015-04-13 20:22:18 -0400

    js added

--- public/js/home.js
+++ public/js/home.js
@@ -285,3 +285,83 @@ function getRanges() {
         }
     });
 }
+
+/**
+ * Makes a call to the apis
+ * @param parameters All parameters being sent to the request
+ * @param callback The callback function
+ */
+function api_request(pars, callback) {
+
+    //Every request starts valid till proven wrong
+    var api_response = null;
+    var validRequest = true;
+    var requestType = 'POST'
+
+    //Check for the request type of POST or GET
+    //This infers that the request type is a POST request
+    //if not specified otherwise
+    if (typeof(pars['request_type'])!='string'){
+        requestType = 'POST'
+    }
+    else {
+        requestType = pars['request_type']
+    }
+
+    //Validate request before proceeding
+    if (!typeof(pars['version']==='string')) { validRequest = false; }
+    if (!typeof(pars['api_name']==='string')) { validRequest = false; }
+    if (!typeof(pars['api_method']==='string')) { validRequest = false; }
+
+    if (validRequest) {
+
+        //Build request url
+        var request_url = window.location.origin + '/addhawk/api/' + pars['api_name'] + '/' +
+            pars['api_method'] + '/param';
+
+        //Turn each key into valid send value
+        for (var key in pars)
+        {
+            var obj = pars[key];
+            if (typeof(pars[key])==='undefined')
+            {
+                pars[key]=obj;
+            }
+        }
+
+        //Create Ajax Request
+        var request = $.ajax({
+            url: request_url,
+            beforeSend: function() {
+
+            },
+            data: pars,
+            dataType: 'json',
+            processData: true,
+            type: requestType
+
+        });
+        request.done(function(data){
+            api_response = data;
+            if (typeof(callback)==='function')
+            {
+                callback(data);
+            }
+        });
+        request.fail(function(data){
+
+        });
+        request.always(function(data){
+
+        });
+        request.then(function(){
+
+        });
+
+        return JSON.parse(api_response);
+    }
+    else {
+        return false;
+    }
+
+}
