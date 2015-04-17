69ac51496e8a0aa41d62df20e1e8a070ad64b27c
ImpulseChimp <cmw2379@vt.edu>
2015-04-17 00:50:03 -0400

    asdf

--- api/models/PixelPusher.php
+++ api/models/PixelPusher.php
@@ -13,10 +13,13 @@ class Pusher implements WampServerInterface {
 
     public function onUnSubscribe(ConnectionInterface $conn, $topic) {
     }
+
     public function onOpen(ConnectionInterface $conn) {
     }
+
     public function onClose(ConnectionInterface $conn) {
     }
+
     public function onCall(ConnectionInterface $conn, $id, $topic, array $params) {
         // In this application if clients send data it's because the user hacked around in console
         $conn->callError($id, $topic, 'You are not allowed to make calls')->close();
