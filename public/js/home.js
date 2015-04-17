658a41665de2a7f3944f66d64295f1fff74d61c1
ImpulseChimp <cmw2379@vt.edu>
2015-04-17 18:27:22 -0400

    asdf

--- public/js/home.js
+++ public/js/home.js
@@ -682,7 +682,8 @@ function registerSeatSocket(row, column) {
               //push from the server for a registerd user.
 
               //Parse Pixels Into display
-							var json_data = String(data.data);
+							var json_data = String(data.data)
+							alert(json_data);
 
               console.log('New Pixel Pushed: "' + topic + '" : ' + json_data);
           });
