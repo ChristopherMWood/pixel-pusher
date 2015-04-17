eeeec3d1f49f565546c626282aa51046dd127c23
ImpulseChimp <cmw2379@vt.edu>
2015-04-17 09:56:17 -0400

    asdf

--- api/models/PixelPusher.php
+++ api/models/PixelPusher.php
@@ -17,11 +17,6 @@ class Pusher implements WampServerInterface {
       public function onBlogEntry($entry) {
           $entryData = json_decode($entry, true);
 
-          // If the lookup topic object isn't set there is no one to publish to
-          if (!array_key_exists($entryData['category'], $this->subscribedTopics)) {
-              return;
-          }
-
           $topic = $this->subscribedTopics[$entryData['category']];
 
           // re-send the data to all the clients subscribed to that category
