d7d7722d6d172ed3b5cb542ac4ce0bef6345f16b
ImpulseChimp <cmw2379@vt.edu>
2015-04-27 13:02:38 -0400

    stuff

--- api/models/PixelPusher.php
+++ api/models/PixelPusher.php
@@ -25,7 +25,7 @@ class Pusher implements WampServerInterface {
 
       //If the lookup topic object isn't set there is no one to publish to
       if (!array_key_exists($entryData['category'], $this->subscribedTopics)) {
-          echo "Subscription not found: ".$entryData['category'];
+          echo "Subscription not found: ".$entryData['category']."\n";
           return;
       }
 
