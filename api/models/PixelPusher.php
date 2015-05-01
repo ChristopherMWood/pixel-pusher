23dcb80f63e3cfdeec15c2ab247db7726044a971
ImpulseChimp <cmw2379@vt.edu>
2015-05-01 14:49:08 -0400

    asdf

--- api/models/PixelPusher.php
+++ api/models/PixelPusher.php
@@ -34,7 +34,7 @@ class Pusher implements WampServerInterface {
       // re-send the data to all the clients subscribed to that category
 //      echo "Broadcasted: ".$entryData['category']." pixels\n";
 
-			echo "Pixel Time: ".(microtime() * 1000)."\n";
+			echo "Pixel Time: ".round(microtime(true) * 1000)."\n";
       $topic->broadcast($entryData);
   }
 
